2bb0222de50738e400c302e33bb41914cfd2b2d7
laueri01 <lau.oi.ching@gmail.com>
2016-03-20 16:53:51 -0400

    New lines of code

--- Titanic02.R
+++ Titanic02.R
@@ -1,6 +1,28 @@
-# Part 1
+# Data exploration/checking
 
 setwd("C:/Users/Eric/Desktop/kaggle")
 
 trainData <- read.csv("train.csv", header = TRUE, stringsAsFactors = FALSE)
 testData <- read.csv("test.csv", header = TRUE, stringsAsFactors = FALSE)
+
+head(trainData)
+
+# View(trainData)
+# View(testData)
+
+# Test plot data
+plot(density(trainData$Age, na.rm = TRUE))
+plot(density(trainData$Fare, na.rm = TRUE))
+
+counts <- table(trainData$Survived, trainData$Sex)
+barplot(counts, xlab = "Gender", ylab = "Number of People", main = "survived and deceased between male and female")
+counts[2] / (counts[1] + counts[2])
+counts[4] / (counts[3] + counts[4])
+
+Pclass_survival <- table(trainData$Survived, trainData$Pclass)
+barplot(Pclass_survival, xlab = "Cabin Class", ylab = "Number of People",
+        main = "survived and deceased between male and female")
+Pclass_survival[2] / (Pclass_survival[1] + Pclass_survival[2])
+Pclass_survival[4] / (Pclass_survival[3] + Pclass_survival[4])
+Pclass_survival[6] / (Pclass_survival[5] + Pclass_survival[6])
+
