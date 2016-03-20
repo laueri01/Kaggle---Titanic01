2dc3ae279b9feefb7a27fb060658b634ca5431b4
laueri01 <lau.oi.ching@gmail.com>
2016-03-20 16:04:41 -0400

    Test: 1st few lines

---
+++ Titanic02.R
@@ -0,0 +1,6 @@
+# Part 1
+
+setwd("C:/Users/Eric/Desktop/kaggle")
+
+trainData <- read.csv("train.csv", header = TRUE, stringsAsFactors = FALSE)
+testData <- read.csv("test.csv", header = TRUE, stringsAsFactors = FALSE)
